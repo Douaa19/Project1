ed5743f093ef40d068ca2b350c9a7d7ea640c9b6
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-10-24 19:09:30 +0100

    CreateMethodForDownloadFiles

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -218,6 +218,33 @@ class CompanyController extends Controller {
     }
 
 
-    
+    // Method For Download Files
+    public function download() {
+        $file = $_POST['file'];
+        download($file);
+        
+    }
+
+    // $name= $_GET['nama'];
+    // download($name);
+
+    // function download($name) {
+    //     $file = $nama_fail;
+
+    //     if (file_exists($file)) {
+    //         header('Content-Description: File Transfer');
+    //         header('Content-Type: application/octet-stream');
+    //         header('Content-Disposition: attachment; filename='.basename($file));
+    //         header('Content-Transfer-Encoding: binary');
+    //         header('Expires: 0');
+    //         header('Cache-Control: must-revalidate');
+    //         header('Pragma: public');
+    //         header('Content-Length: ' . filesize($file));
+    //         ob_clean();
+    //         flush();
+    //         readfile($file);
+    //         exit;
+    //     }
+    // }
 
 }
--- app/views/company/homePage.php
+++ app/views/company/homePage.php
@@ -17,10 +17,34 @@
                 <tbody>
                     <?php foreach($data as $company) : ?>
                         <tr>
-                        <td><a href="<?= URLROOT ?>/uploads/<?= $company->contract; ?>" class="text-uppercase text-decoration-none">contract</a></td>
-                        <td><a href="<?= URLROOT ?>/uploads/<?= $company->contract_salarie; ?>" class="text-uppercase text-decoration-none">contract salarie</a></td>
-                        <td><a href="<?= URLROOT ?>/uploads/<?= $company->facture; ?>" class="text-uppercase text-decoration-none">facture</a></td>
-                        <td><a href="<?= URLROOT ?>/uploads/<?= $company->liste_personnel; ?>" class="text-uppercase text-decoration-none">liste du personnel</a></td>
+                            <td>
+                                <form action="<?php echo URLROOT; ?>/CompanyController/download" method="post">
+                                    <input type="hidden" name="file" value="<?= $company->contract; ?>">
+                                    <button type="submit" class="">contract</button>
+                                </form>
+                            </td>
+                            <td>
+                                <form action="<?php echo URLROOT; ?>/CompanyController/download" method="post">
+                                    <input type="hidden" name="file" value="<?= $company->contract_salarie; ?>">
+                                    <button type="submit" class="">contract salarie</button>
+                                </form>
+                            </td>
+                            <td>
+                                <form action="<?php echo URLROOT; ?>/CompanyController/download" method="post">
+                                    <input type="hidden" name="file" value="<?= $company->facture; ?>">
+                                    <button type="submit" class="">facture</button>
+                                </form>
+                            </td>
+                            <td>
+                                <form action="<?php echo URLROOT; ?>/CompanyController/download" method="post">
+                                    <input type="hidden" name="file" value="<?= $company->liste_personnel; ?>">
+                                    <button type="submit" class="">liste du personnel</button>
+                                </form>
+                            </td>
+                            <!-- <td><a href="<?= URLROOT ?>/uploads/<?= $company->contract; ?>" class="text-uppercase text-decoration-none">contract</a></td> -->
+                            <!-- <td><a href="<?= URLROOT ?>/uploads/<?= $company->contract_salarie; ?>" class="text-uppercase text-decoration-none">contract salarie</a></td>
+                            <td><a href="<?= URLROOT ?>/uploads/<?= $company->facture; ?>" class="text-uppercase text-decoration-none">facture</a></td>
+                            <td><a href="<?= URLROOT ?>/uploads/<?= $company->liste_personnel; ?>" class="text-uppercase text-decoration-none">liste du personnel</a></td> -->
                     </tr>
                     <?php endforeach; ?>
                 </tbody>
